848152703c54e39e2c653d78ed81117f7c99679c
matteocavallo <mcavallo@soldo.com>
2021-08-17 18:58:19 +0200

    Added Dependencies

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import  Icon  from 'react-native-vector-icons/Ionicons'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={() => alert("Ciao")}>
+        <Icon name="person-circle-outline" size={32} color="green"/>
+      </TouchableOpacity>
     </View>
   );
 }
